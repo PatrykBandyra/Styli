64b05a7cf6c1be11ebff08c4c135a439b458fe2b
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-11-07 00:12:12 +0100

    docker - mysql

--- styli-ui/src/app/image.service.ts
+++ styli-ui/src/app/image.service.ts
@@ -9,7 +9,22 @@ export class ImageService {
     constructor(private http: HttpClient) {
     }
 
-    uploadImage() {
+    private static readonly BASE_URL: string = '/api/image';
 
+    uploadImage(image: File, description?: string) {
+        const form = new FormData();
+        form.append('image', image, image.name);
+        form.append('imageDetails',
+            new Blob([
+                JSON.stringify({
+                    description: description ? description : '',
+                })], {type: 'application/json'},
+            ),
+        );
+        return this.http.post(ImageService.BASE_URL, form);
     }
+
+    // getImages(page: number, size: number) {
+    //     this.http.get<>
+    // }
 }
