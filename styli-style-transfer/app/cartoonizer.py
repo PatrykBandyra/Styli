a68e34409b18679c97f280bb90f0fce6b3a9fea5
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-11-07 23:39:22 +0100

    docker - py

--- styli-style-transfer/app/cartoonizer.py
+++ styli-style-transfer/app/cartoonizer.py
@@ -14,7 +14,7 @@ import network
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s')
-file_handler = logging.FileHandler('log/cartoonization.log', mode='w')
+file_handler = logging.FileHandler('cartoonization.log', mode='w')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
--- styli-style-transfer/app/main.py
+++ styli-style-transfer/app/main.py
@@ -14,7 +14,7 @@ from cartoonizer import Cartoonizer
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s')
-file_handler = logging.FileHandler('log/app.log', mode='w')
+file_handler = logging.FileHandler('app.log', mode='w')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
