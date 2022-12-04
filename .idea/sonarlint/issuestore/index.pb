36972ef6e2ed6dfbac864cee7b408914598ca623
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-12-04 23:42:40 +0100

    colorize micro service

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,23 +3,25 @@ v
 Fstyli-api/src/main/kotlin/styli/effects/dto/response/EffectResponse.kt,3\b\3b1d076cd0f7e12e0ef3fe0dac43463db0ec244e
 u
 Estyli-ui/src/app/effects/background-swap/background-swap.component.ts,3\4\34c2883a533c4cf1792b352d932620b3cbfb3468
-B
-style-transfer.env,1\c\1c539426ae5f012a7aa9a6c80e183f817c3fd266
 V
 &styli-segmentation/app/segmentation.py,3\a\3a604386b37e73b4c91a56ba834a21aaace3012f
 6
 db.env,8\3\83174910b0381877ae8df0dc5c5f452bfdb6858a
 N
 styli-segmentation/app/main.py,7\e\7e2d8159c705bbd9366b21f02ea217127872ab1a
-C
-docker-compose.yaml,e\f\ef8def03dc8f162799089e9b5f4321201c8810d5
-Q
-!styli-style-transfer-2/Dockerfile,0\f\0fb6df2d6aaf602536b9bdc073a0d5017890bc3b
 a
 1styli-style-transfer-2/styli-style-transfer-2.iml,9\c\9ce2786925edda46b86b69003a5101f8e2b18ddd
-Y
-)styli-style-transfer-2/app/test_main.http,3\0\30d32ae7266652e8a87e2a8a0cbe5941b715ecbf
 N
 styli-segmentation/app/app.log,a\d\ad163e61e73c6ca1731cbec2ef615c1c61853355
-R
-"styli-style-transfer-2/app/main.py,0\a\0ad25685e64fbcce8372e62edab1bf36f34ab9ed
+4
+.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+q
+Astyli-api/src/main/kotlin/styli/effects/service/EffectsService.kt,3\8\38d535bb75225e7bee7a133959ee5e9d5b7195c4
+t
+Dstyli-api/src/main/kotlin/styli/effects/dto/request/EffectRequest.kt,6\a\6a22409804d92a0213e0fa2a63c242745a6aa576
+{
+Kstyli-android/app/src/main/java/styli/android/api/dto/image/ImageDetails.kt,6\c\6c8b71cb64e1b640f1de5e072ec9ebc26c77b341
+h
+8styli-android/app/src/main/java/styli/android/api/Api.kt,a\9\a983cf8fca2d0c7cbd80ba0fcb8aa19d03545c1d
+a
+1styli-android/app/src/main/res/values/strings.xml,3\8\38388f33eac6635167324a7f3fe9c50072cd55ab
--- styli-style-transfer-2/app/main.py
+++ styli-style-transfer-2/app/main.py
@@ -1,13 +1,91 @@
-from fastapi import FastAPI
+import contextlib
+import logging
+import os
+from io import BytesIO
+
+import pystiche
+import torch
+import uvicorn
+from PIL import Image
+from fastapi import FastAPI, UploadFile, File
+from fastapi.exceptions import HTTPException
+from fastapi.responses import StreamingResponse
+from pystiche.image.io import import_from_pil, export_to_pil
+from torchvision import transforms
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s')
+file_handler = logging.FileHandler('app.log', mode='w')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 app = FastAPI()
+ROOT_URL = '/st2'
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_state_dict():
+    try:
+        return torch.load('example_transformer.pth')
+
+    except FileNotFoundError:
+        @contextlib.contextmanager
+        def suppress_output():
+            with open(os.devnull, 'w') as devnull:
+                with contextlib.redirect_stdout(devnull), contextlib.redirect_stderr(devnull):
+                    yield
+
+        url = "https://download.pystiche.org/models/example_transformer.pth"
+
+        with suppress_output():
+            return torch.hub.load_state_dict_from_url(url)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+transformer = pystiche.demo.transformer().to(device)
+transformer.load_state_dict(get_state_dict())
+transformer.eval()
+
+
+def perform_nst(input_image: Image) -> torch.Tensor:
+    image_tensor = transforms.ToTensor()(input_image)
+    print(f'Image tensor size: {image_tensor.size()}')
+    if len(list(image_tensor.size())) == 4:
+        image_tensor = image_tensor.squeeze(0)
+    image_tensor.to(device)
+    print(f'Image tensor size: {image_tensor.size()}')
+
+    with torch.no_grad():
+        output_image_tensor = transformer(image_tensor)
+    return output_image_tensor
+
+
+@app.post(f'{ROOT_URL}/nst')
+async def neural_style_transfer(image: UploadFile = File(...)):
+    if image.content_type not in ['image/jpeg', 'image/png']:
+        raise HTTPException(400, detail='Invalid file type')
+
+    original_image = Image.open(image.file)
+    original_image_tensor = import_from_pil(original_image, device=device, make_batched=True)
+    nst_image = transformer(original_image_tensor)
+    pillow_nst_image = export_to_pil(nst_image)
+
+    response_image = BytesIO()
+    pillow_nst_image.save(response_image, 'JPEG')
+    response_image.seek(0)
+
+    return StreamingResponse(response_image, media_type='image/jpeg')
+
+
+@app.get(f'{ROOT_URL}/health')
+async def health_check() -> str:
+    logger.info('/health endpoint called')
+    return '[ST2]: Health check works'
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8003)
